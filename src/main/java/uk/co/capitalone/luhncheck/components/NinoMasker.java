01c10e2c8e2365b46bdd0ad1467c967bbe9a9f98
Capital-One-University-Workshop-Kent <93624290+Capital-One-University-Workshop-Kent@users.noreply.github.com>
2021-11-03 10:07:59 +0000

    Create NinoMasker.java

---
+++ src/main/java/uk/co/capitalone/luhncheck/components/NinoMasker.java
@@ -0,0 +1,15 @@
+package uk.co.capitalone.luhncheck.components;
+
+public class NinoMasker {
+
+  public boolean validNationalInsuranceNumber(String nino){
+    return true;
+  }
+
+  public String maskNino(String nino){
+    return "";
+  }
+
+  public String maskNinoInString(String ninoString){
+    return "";
+  }
