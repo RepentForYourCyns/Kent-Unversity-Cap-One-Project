9af62016abb3deaf0eb794faed0258cdbb1ede20
Capital-One-University-Workshop-Kent <93624290+Capital-One-University-Workshop-Kent@users.noreply.github.com>
2021-11-03 10:07:30 +0000

    Create CreditCardValidator.java

---
+++ src/main/java/uk/co/capitalone/luhncheck/components/CreditCardValidator.java
@@ -0,0 +1,18 @@
+package uk.co.capitalone.luhncheck.components;
+
+public class CreditCardValidator {
+
+
+    public boolean luhnCheck(String creditCardNumber){
+        return true;
+    }
+
+    public int luhnSum(String creditCardNumber){
+        return 0;
+    }
+
+    public boolean isValidCardNumberLength(String creditCardNumber){
+        return true;
+    }
+
+}
